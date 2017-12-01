c0e7e024bda2d6ecf87d22015c135b74d1846688
wangshengchen <wangshengchen@git.meican-inc.com>
2017-12-01 11:06:03 +0800

    add: 1、增加了医院基本信息表； fix: 1、在用户表中增加了医院ID和医院NAME的字段

---
+++ easy/db/mysql/轻松筹/bangnichou_newTable/t_user.sql
@@ -0,0 +1,50 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : bangni
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : bangnichou
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2017-12-01 10:31:58
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `user_id` varchar(25) NOT NULL COMMENT '用户ID',
+  `user_name` varchar(30) NOT NULL COMMENT '用户登录用户名(此刻为手机号)',
+  `hospital_id` varchar(25) DEFAULT NULL,
+  `hospital_name` varchar(50) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL COMMENT '用户登录密码',
+  `trade_pwd` varchar(25) DEFAULT NULL COMMENT '交易密码',
+  `mobile` varchar(11) DEFAULT NULL COMMENT '手机号码',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `source` char(2) NOT NULL COMMENT '1 PC \r\n       2 微信\r\n       3 安卓\r\n       4 IOS',
+  `source_type` char(2) DEFAULT NULL COMMENT '1产品众筹\r\n            2股权众筹  \r\n     3房产众筹       4轻松筹',
+  `date_register` datetime NOT NULL COMMENT '注册时间',
+  `user_status` char(2) NOT NULL DEFAULT '1' COMMENT '1正常\r\n            2锁定\r\n            3 拉黑',
+  `date_login` datetime DEFAULT NULL,
+  `date_last_login` datetime DEFAULT NULL COMMENT '最后一次登录时间',
+  `first_login` char(2) DEFAULT NULL COMMENT '1第一次\r\n            2不是第一次',
+  `last_login_ip` varchar(30) NOT NULL COMMENT '最后一次登录ip',
+  `pwd_error_count` int(2) DEFAULT NULL COMMENT '当日交易密码错误次数',
+  `fail_login_count` int(2) DEFAULT NULL COMMENT '当日登录失败次数',
+  `user_type` char(2) NOT NULL DEFAULT '2' COMMENT '1医院用户\n2个人用户',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户总表';
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('101711251707099505', '13693350621', '101711251707099505', '千佛山', null, null, '13693350621', null, '1', '4', '2017-11-25 17:07:09', '1', '2017-11-30 19:12:50', '2017-11-30 19:11:13', '2', '10.0.0.233', null, null, '1');
+INSERT INTO `t_user` VALUES ('101711271806007644', '18653181412', '101711271806007644', '大明湖', null, null, '18653181412', null, '1', '4', '2017-11-27 18:06:00', '1', '2017-11-30 00:05:00', '2017-11-30 00:02:43', '2', '192.168.31.12', null, null, '1');
