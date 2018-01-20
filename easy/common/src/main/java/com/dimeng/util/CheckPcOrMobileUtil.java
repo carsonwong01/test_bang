30e477b495af01646e72fb63e876d55f45d2612f
wangshengchen <wangshengchen@git.meican-inc.com>
2018-01-20 16:19:23 +0800

    fix:

--- easy/common/src/main/java/com/dimeng/util/CheckPcOrMobileUtil.java
+++ easy/common/src/main/java/com/dimeng/util/CheckPcOrMobileUtil.java
@@ -35,35 +35,4 @@ public class CheckPcOrMobileUtil {
         }
         return isMoblie;
     }
-
-
-
-
-
-
-
-
-
-
-
-//    private final static String[] agent =
-//            { "Android", "iPhone", "iPod","iPad", "Windows Phone", "MQQBrowser" }; //定义移动端请求的所有可能类型
-//    /**
-//     * 判断User-Agent是不是来自于手机
-//     */
-//    public static boolean checkAgentIsMobile(String ua){
-//        boolean flag = false;
-//        if(!ua.contains("Windows NT") || (ua.contains("Windows NT") && ua.contains("compatible; MSIE 9.0;"))){
-//        //排除 苹果桌面系统
-//            if(!ua.contains("Windows NT") && !ua.contains("Macintosh")){
-//                for(String item : agent){
-//                    if(ua.contains(item)){
-//                        flag = true;
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//        return flag;
-//    }
 }
